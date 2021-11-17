a940af1228869ab114669fd1d79610d3adca281f
austin espinal <austinespi@gmail.com>
2021-11-16 21:16:30 -0500

    add repo search. working on repo name, issue count, and username acquision

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,7 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
 var getUserRepos = function(user) {
     //format the git hub api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,5 +14,18 @@ var getUserRepos = function(user) {
     });
 };
 
-console.log("outside");
-getUserRepos();
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    //get value from input element
+    var username = nameInputEl.ariaValueMax.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a Github username");
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
